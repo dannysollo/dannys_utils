b36690d3c1a6fbd40200bd0286b60b0501e26b00
dannysollo <dgomez1270a@gmail.com>
2024-02-05 08:04:53 -0500

    Initial commit

---
+++ src/battery_monitor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.6
+
+import rospy
+import numpy as np
+from std_msgs.msg import Float32
+from std_msgs.msg import String
+import time
+from subprocess import call
+
+
+# Monitor /battery topic - if voltage is 14V or below, publish battery fault on /status
+
+threshold_voltage = 14.0
+
+def callback_battery(data_battery):
+    if data_battery.data <= threshold_voltage:
+        pub = rospy.Publisher('/status', String, queue_size=10)
+        pub.publish('FAULT-BATTERY')
+        time.sleep(5)
+    pass
+def battery_monitor():
+    rospy.init_node('battery_monitor', anonymous=True)
+    rospy.Subscriber("battery", Float32, callback_battery)
+    rospy.spin()
+
+if __name__ == '__main__':
+    battery_monitor()
---
+++ src/lidar_process.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3.6
+
+import rospy
+import numpy as np
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import String
+import time
+
+
+# Subscribe to Lidar data from rplidar. 
+# Prune certain angles - currently include all as this requires testing.
+# Determine the closest distance to the robot and the angle at which it occurs.
+# If the closest distance is less than a threshold, send a fault on robotstatus topic.
+
+def callback(data):
+    # Range of angles to ignore
+    mindist = 0.5
+    ignored_angles = [[0, 0], [0, 0]]
+    ranges = np.array(data.ranges)
+    # Prune the ranges
+    for i in range(len(ignored_angles)):
+        ranges[ignored_angles[i][0]:ignored_angles[i][1]] = 100
+    # Find the minimum distance and the angle at which it occurs
+    min_distance = np.min(ranges)
+    min_angle = np.argmin(ranges)
+    if (min_distance < mindist):
+        print("FAULT-PROXIMITY")
+        pub = rospy.Publisher('status', String, queue_size=10)
+        pub.publish("FAULT-PROXIMITY")
+        time.sleep(10)
+
+
+def lidar_node():
+    rospy.init_node('lidar_node', anonymous=True)
+    rospy.Subscriber("/scan", LaserScan, callback)
+    rospy.spin()
+    
+
+
+if __name__ == '__main__':
+    lidar_node()
